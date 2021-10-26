0ed90cd623cf2919d5dd298bdaa08edd1df5dcdf
3commascapital <3commascapital@gmail.com>
2021-10-26 15:21:23 -0500

    converted operation name to fieldname

--- src/index.ts
+++ src/index.ts
@@ -14,25 +14,25 @@ export const register = (rep: Reporter = reporter) => {
   rep.metric('resolved', new promClient.Counter({
     name: 'graphql_queries_resolved',
     help: 'The amount of GraphQL queries that have had their operation resolved.',
-    labelNames: ['operationName', 'operation'],
+    labelNames: ['fieldName', 'operation'],
     registers,
   }))
   rep.metric('startedExecuting', new promClient.Counter({
     name: 'graphql_queries_execution_started',
     help: 'The amount of GraphQL queries that have started executing.',
-    labelNames: ['operationName', 'operation'],
+    labelNames: ['fieldName', 'operation'],
     registers,
   }))
   rep.metric('encounteredErrors', new promClient.Counter({
     name: 'graphql_queries_errored',
     help: 'The amount of GraphQL queries that have encountered errors.',
-    labelNames: ['operationName', 'operation'],
+    labelNames: ['fieldName', 'operation'],
     registers,
   }))
   rep.metric('responded', new promClient.Counter({
     name: 'graphql_queries_responded',
     help: 'The amount of GraphQL queries that have been executed and been attempted to send to the client. This includes requests with errors.',
-    labelNames: ['operationName', 'operation'],
+    labelNames: ['fieldName', 'operation'],
     registers,
   }))
   rep.metric('resolverTime', new promClient.Histogram({
@@ -50,7 +50,7 @@ export const register = (rep: Reporter = reporter) => {
   rep.metric('totalRequestTime', new promClient.Histogram({
     name: 'graphql_total_request_time',
     help: 'The time to complete a GraphQL query.',
-    labelNames: ['operationName', 'operation'],
+    labelNames: ['fieldName', 'operation'],
     registers,
   }))
 }
